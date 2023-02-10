50df5a39e406ea15717bef78897c9192e49b8b88
Hazel <hazeldoom520@gmail.com>
2023-02-10 13:46:29 +0630

    initial commit

--- src/components/Layouts/NavBar.vue
+++ src/components/Layouts/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
       <nav class="navbar navbar-expand-md navbar-dark bg-olive mb-2 mb-lg-4  fixed-top overflow-hidden">
-        <router-link class="navbar-brand" to="/">The MOVIE.db</router-link>
+        <router-link class="navbar-brand" to="/">S-Movie</router-link>
         <button
           class="navbar-toggler"
           type="button"
@@ -22,13 +22,13 @@
               <a class="nav-link" @click="flash('This Page is Under Maintainence')">Popular</a>
             </li>
             <li class="nav-item px-md-2 px-lg-3">
-              <router-link to="/genres" class="nav-link">Genres</router-link>
+              <router-link @click="flash('This Page is Under Maintainence')" class="nav-link">Genres</router-link>
             </li>
             <li class="nav-item px-md-2 px-lg-3">
               <a class="nav-link" @click="flash('Error','This Page is Under Maintainence')">Trending</a>
             </li>
             <li class="nav-item px-md-2 px-lg-3">
-              <router-link to="/about" class="nav-link">About</router-link>
+              <router-link @click="flash('This Page is Under Maintainence')" class="nav-link">About</router-link>
             </li>
           </ul>
         </div>
